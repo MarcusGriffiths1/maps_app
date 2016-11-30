f714efbdc4ab7adf3568f06507be6239b4f8591a
MarcusG <marcusguygriffiths@gmail.com>
2016-11-30 12:19:06 +0000

    added MapsApp object to global scope

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
 import MapsApp from './MapsApp/MapsApp';
-import { places, mainMarker } from './data/mockdata';
 
-const options = {
-	infoWindow: true,
-	customMarkers: {
-		path: './img/amenity_icons/',
-		zoom: '_icon_large',
-		icon: '_icon_small'
-	},
-	showDistance: true,
-	sortBy: ['type', 'distance']
-};
-
-var myMap = new MapsApp('map', mainMarker, places, options);
-myMap.createList('js-amenity-list');
-myMap.createFilter('js-filter-controls');
-myMap.createSorter('js-sort-list');
+window.MapsApp = MapsApp;
